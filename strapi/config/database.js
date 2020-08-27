476bee3f121c5be103de69595f0cb8105e91452b
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-27 15:28:35 +0700

    strapi - split database config into environment

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,15 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', 'postgres'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {}
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
