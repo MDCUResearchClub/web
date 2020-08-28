8a326e18495a058b15f41e04cef6e14cb6061b92
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-28 12:39:51 +0700

    nextjs - add gtag

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,7 +1,20 @@
-import '../lib/globals.css'
+import { useEffect } from "react";
+import Router from "next/router";
+import { pageview } from "../lib/gtag";
+import "../lib/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
