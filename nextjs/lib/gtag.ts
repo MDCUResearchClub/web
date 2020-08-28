d07c31abdfc86123f51c7a1e1f35d5f6ac28ac48
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-28 21:21:51 +0700

    nextjs - fix gtag

--- nextjs/lib/gtag.ts
+++ nextjs/lib/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = process.env.GA_TRACKING_ID || "G-G832CFVLYM";
+export const GA_TRACKING_ID = process.env.NODE_ENV === "production" ?  "G-HDHM4V8SR6" : "G-G832CFVLYM";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
