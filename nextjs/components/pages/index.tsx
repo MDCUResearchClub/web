acb6bdec6d369049fb4756dd29202ee8f3e61021
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-28 20:07:58 +0700

    nextjs - update page design

--- nextjs/components/pages/index.tsx
+++ nextjs/components/pages/index.tsx
@@ -8,12 +8,12 @@ export default function IndexPage({ message = "" }) {
   return (
     <Page title="Home">
       {message ? messageBox : null}
-      <div className="p-4 md:p-8 container relative mx-auto md:flex justify-around items-start">
-        <div className="flex flex-col justify-center absolute md:static top-0 bottom-0 right-0 opacity-50 md:opacity-100 self-center w-1/2 md:w-5/12 lg:w-4/12 headline-img px-4">
+      <div className="p-4 sm:p-6 md:p-8 container relative mx-auto md:flex justify-around items-start">
+        <div className="flex flex-col justify-center absolute md:static top-0 bottom-0 right-0 self-center w-1/2 md:w-5/12 lg:w-4/12 headline-img px-4">
           <img alt="" src="/images/front.svg" />
         </div>
-        <div className="md:bg-blue-200 md:p-12 lg:p-16 xl:p-20 my-12 sm:m-8 headline-container">
-          <h1 className="text-3xl md:text-4xl xl:text-5xl leading-relaxed mb-8">
+        <div className="md:bg-blue-200 md:p-8 lg:p-12 xl:p-16 my-20 sm:mx-8 headline-container">
+          <h1 className="text-3xl md:text-4xl xl:text-5xl leading-relaxed mb-24 md:mb-8">
             Research is
             <br />
             <span className="whitespace-no-wrap text-4xl md:text-5xl xl:text-6xl relative headline-highlight z-0">
@@ -40,11 +40,32 @@ export default function IndexPage({ message = "" }) {
           .headline-img {
             transform: scaleX(-1);
             z-index: -1;
+            animation: headline-img .5s ease-out;
+          }
+
+
+          @keyframes headline-img {
+            from {
+              transform: scaleX(-1) translateY(1rem);
+              opacity: .25;
+            }
           }
 
           @screen md {
             .headline-img {
               @apply transform scale-x-100;
+            }
+            @keyframes headline-img {
+              from {
+                transform: translateY(2rem);
+                opacity: .25;
+              }
+            }
+          }
+
+          @media (prefers-reduced-motion: reduce) {
+            .headline-img {
+              animation: none;
             }
           }
         `}
--- nextjs/components/pages/talks.tsx
+++ nextjs/components/pages/talks.tsx
@@ -9,10 +9,10 @@ export default function TalksPage({ message = "" }) {
     <Page title="Home">
       {message ? messageBox : null}
       <div className="p-4 md:p-8 container relative mx-auto md:flex justify-around items-start">
-        <div className="flex flex-col justify-end absolute md:static top-0 bottom-0 right-0 opacity-50 md:opacity-100 self-center w-1/2 md:w-5/12 lg:w-4/12 headline-img px-4">
+        <div className="flex flex-col justify-end absolute md:static top-0 bottom-0 right-0 self-center w-1/2 md:w-5/12 lg:w-4/12 headline-img px-4">
           <img alt="" src="/images/peep.svg" />
         </div>
-        <div className="md:bg-blue-200 md:p-12 lg:p-16 xl:p-20 my-32 sm:mx-8 headline-container">
+        <div className="md:bg-blue-200 md:p-8 lg:p-12 xl:p-16 my-32 sm:mx-8 headline-container">
           <h1 className="text-3xl md:text-4xl xl:text-5xl leading-relaxed mb-8">
             Research Talks are
             <br />
@@ -37,11 +37,31 @@ export default function TalksPage({ message = "" }) {
           .headline-img {
             transform: scaleX(-1);
             z-index: -1;
+            animation: headline-img .5s ease-out;
+          }
+
+          @keyframes headline-img {
+            from {
+              transform: scaleX(-1) translateY(1rem);
+              opacity: .25;
+            }
           }
 
           @screen md {
             .headline-img {
               @apply transform scale-x-100;
+            }
+            @keyframes headline-img {
+              from {
+                transform: translateY(2rem);
+                opacity: .25;
+              }
+            }
+          }
+
+          @media (prefers-reduced-motion: reduce) {
+            .headline-img {
+              animation: none;
             }
           }
         `}
