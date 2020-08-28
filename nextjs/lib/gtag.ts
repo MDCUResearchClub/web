69632fecbf341851359585ddb6ad3c5970a570df
ThewBear <26767607+ThewBear@users.noreply.github.com>
2020-08-29 00:35:27 +0700

    nextjs - set gtag through raw html

--- nextjs/lib/gtag.ts
+++ nextjs/lib/gtag.ts
@@ -1,4 +1,16 @@
-export const GA_TRACKING_ID = process.env.NODE_ENV === "production" ?  "G-HDHM4V8SR6" : "G-G832CFVLYM";
+export const GA_TRACKING_ID =
+  process.env.NODE_ENV === "production" ? "G-HDHM4V8SR6" : "G-G832CFVLYM";
+
+export const HEAD_SCRIPT = `
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${GA_TRACKING_ID}');
+</script>`;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
+import { HEAD_SCRIPT } from "../lib/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -36,21 +36,12 @@ class MyDocument extends Document {
             name="google-site-verification"
             content="IZCRXZBnxXQG-WklRrjvGosyO69S8WLnMvyj6OChCkQ"
           />
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
+              </script>
+              ${HEAD_SCRIPT}
+              <script>
           `,
             }}
           />
