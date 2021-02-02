25222b2725cc5d9e752ed064ca421cd0a37534d2
Thew Dhanat <26767607+ThewBear@users.noreply.github.com>
2021-02-02 14:15:18 +0700

    nextjs - add open graph to newsItem (#182)

--- nextjs/components/pages/newsItem.tsx
+++ nextjs/components/pages/newsItem.tsx
@@ -1,10 +1,11 @@
 import { GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import Head from "next/head";
 import ReactMarkdown from "react-markdown";
 import unwrapImages from "remark-unwrap-images";
 
-import { STRAPI_ENDPOINT } from "../../lib/constant";
+import { SITE_NAME, SITE_ORIGIN, STRAPI_ENDPOINT } from "../../lib/constant";
 import Page from "../Page";
 import { useStrapi, fetchStrapiPublic } from "../../lib/strapi";
 
@@ -34,13 +35,35 @@ export default function NewsPage({ staticNewsItem }) {
     image: ImageRenderer(finalNewsItem ? finalNewsItem["bodyImages"] : {}),
   };
 
+  const ogImage =
+    finalNewsItem &&
+    (finalNewsItem.preview?.url || finalNewsItem.cover?.url || "");
+
   return (
     <Page
-      title="News"
+      title={finalNewsItem ? finalNewsItem.title : "News"}
       description={finalNewsItem ? finalNewsItem.description : ""}
     >
       {finalNewsItem && (
         <div className="px-2 md:px-4 lg:px-16 container mx-auto py-4">
+          <Head>
+            <meta
+              property="og:url"
+              content={SITE_ORIGIN + "/news/" + router.query.id}
+            />
+            <meta property="og:type" content="website" />
+            <meta
+              property="og:title"
+              content={finalNewsItem.title + " | " + SITE_NAME}
+            />
+            <meta
+              property="og:description"
+              content={finalNewsItem.description}
+            />
+            {ogImage && (
+              <meta property="og:image" content={STRAPI_ENDPOINT + ogImage} />
+            )}
+          </Head>
           <h1 className="font-serif text-3xl mb-4 text-center">
             {finalNewsItem.title}
           </h1>
