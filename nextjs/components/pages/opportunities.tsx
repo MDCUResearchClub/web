14a822d8e4232e0aa4c819722c14b44d31f1a9f0
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-02-08 22:01:24 +0700

    Add opportunities

---
+++ nextjs/components/pages/opportunities.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+import Page from "../Page";
+import { useStrapi } from "../../lib/strapi";
+
+function OpportunitiesCard({ opportunity }) {
+  return  <Link href={`/opportunities/${opportunity.id}`}>
+  <a className="block">
+    <h3 className="my-4 text-xl">{opportunity.title}</h3>
+  </a>
+</Link>
+}
+
+export default function OpportunitiesPage() {
+  const { data: opportunities } = useStrapi("/opportunities");
+  function getOpportunitiesCard(opportunity) {
+    return <OpportunitiesCard opportunity={opportunity} />;
+  }
+
+  const opportunitiesCard = opportunities
+    ? opportunities.map(getOpportunitiesCard)
+    : null;
+
+  return (
+    <Page title="Opportunities">
+      <div className="px-2 md:px-4 lg:px-16 container mx-auto py-4">
+        <h1 className="font-serif text-3xl mb-4 text-center">
+          Research Opportunities
+        </h1>
+        <div className="grid md:grid-cols-2">{opportunitiesCard}</div>
+      </div>
+    </Page>
+  );
+}
---
+++ nextjs/components/pages/opportunityItem.tsx
@@ -0,0 +1,47 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import ReactMarkdown from "react-markdown";
+
+import { SITE_NAME, SITE_ORIGIN } from "../../lib/constant";
+import Page from "../Page";
+import { useStrapi } from "../../lib/strapi";
+
+export default function OpportunityPage() {
+  const router = useRouter();
+  const { data: opportunityItem, dataError } = useStrapi(
+    `/opportunities/${router.query.id}`
+  );
+  if (dataError) {
+    router.replace("/");
+  }
+
+  return (
+    <Page
+      title={opportunityItem ? opportunityItem.title : "Opportunities"}
+    >
+      {opportunityItem && (
+        <div className="px-2 md:px-4 lg:px-16 container mx-auto py-4">
+          <Head>
+            <meta
+              property="og:url"
+              content={SITE_ORIGIN + "/opportunities/" + router.query.id}
+            />
+            <meta property="og:type" content="website" />
+            <meta
+              property="og:title"
+              content={opportunityItem.title + " | " + SITE_NAME}
+            />
+          </Head>
+          <h1 className="font-serif text-3xl mb-8 text-center">
+            {opportunityItem.title}
+          </h1>
+          <div className="prose mx-auto">
+            <ReactMarkdown>
+              {opportunityItem.details}
+            </ReactMarkdown>
+          </div>
+        </div>
+      )}
+    </Page>
+  );
+}
