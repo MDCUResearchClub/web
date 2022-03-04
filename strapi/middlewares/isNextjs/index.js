865d0554a4dce83bfa680f5d68d96afd6d4b80a5
ThewBear <26767607+ThewBear@users.noreply.github.com>
2022-03-05 00:35:28 +0700

    strapi - verify isNextjs permission

--- strapi/middlewares/isNextjs/index.js
+++ strapi/middlewares/isNextjs/index.js
@@ -16,6 +16,28 @@ module.exports = (strapi) => {
             ].services.user.fetch({
               email: "nextjs@mdcuresearchclub.thew.pro",
             });
+
+            // verify permission
+            // source: packages/strapi-plugin-users-permissions/config/policies/permissions.js
+            const role = ctx.state.user.role;
+            const route = ctx.request.route;
+            const permission = await strapi
+              .query("permission", "users-permissions")
+              .findOne(
+                {
+                  role: role.id,
+                  type: route.plugin || "application",
+                  controller: route.controller,
+                  action: route.action,
+                  enabled: true,
+                },
+                []
+              );
+
+            if (!permission) {
+              return ctx.forbidden("You're not Nextjs!");
+            }
+
             return await next();
           }
           // wrong nextjs token
