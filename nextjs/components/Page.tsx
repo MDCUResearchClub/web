f3010d726d63da1a9fdb27d42d8ff55bbb6e1859
ThewBear <26767607+ThewBear@users.noreply.github.com>
2022-12-31 11:55:40 +0700

    Fix title warnings

--- nextjs/components/Page.tsx
+++ nextjs/components/Page.tsx
@@ -3,14 +3,15 @@ import Nav from "./common/Nav";
 import Footer from "./common/Footer";
 import { SITE_NAME } from "../lib/constant";
 
-export default function Page({ title = "", description = "MDCU Research Club", children }) {
+export default function Page({
+  title = "",
+  description = "MDCU Research Club",
+  children,
+}) {
   return (
     <div className="flex flex-col min-h-screen">
       <Head>
-        <title>
-          {title ? `${title} | ` : ""}
-          {SITE_NAME}
-        </title>
+        <title>{title ? `${title} | ${SITE_NAME}` : SITE_NAME}</title>
         <meta name="description" content={description} />
       </Head>
       <Nav />
