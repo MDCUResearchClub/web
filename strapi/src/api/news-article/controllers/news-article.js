1402be02989c99861d25c9403d93ab7c1283a02c
ThewBear <26767607+ThewBear@users.noreply.github.com>
2023-01-15 20:54:52 +0700

    Strapi - remove public attribute

--- strapi/src/api/news-article/controllers/news-article.js
+++ strapi/src/api/news-article/controllers/news-article.js
@@ -5,36 +5,5 @@
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
-const { ForbiddenError } = require('@strapi/utils').errors;
 
-module.exports = createCoreController(
-  "api::news-article.news-article",
-  ({ strapi }) => ({
-    // Method 2: Wrapping a core action (leaves core logic in place)
-    async find(ctx) {
-      // some custom logic here
-      if (!ctx.state.user) {
-        ctx.query = {
-          ...ctx.query,
-          filters: { ...ctx.query.filters, public: true },
-        };
-      }
-
-      // Calling the default core action
-      const { data, meta } = await super.find(ctx);
-
-      return { data, meta };
-    },
-
-    async findOne(ctx) {
-      // Calling the default core action
-      const { data, meta } = await super.findOne(ctx);
-
-      if (!ctx.state.user && !data.attributes.public) {
-        throw new ForbiddenError('You are not allowed to access this article');
-      }
-
-      return { data, meta };
-    },
-  })
-);
+module.exports = createCoreController("api::news-article.news-article");
