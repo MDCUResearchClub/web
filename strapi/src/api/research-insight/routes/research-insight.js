f4b326e32b2745c734b6e77515aa2d55af7864d5
ThewBear <26767607+ThewBear@users.noreply.github.com>
2023-01-16 18:44:19 +0700

    Add research-insight collection

---
+++ strapi/src/api/research-insight/routes/research-insight.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * research-insight router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::research-insight.research-insight');
---
+++ strapi/src/api/research-insight/services/research-insight.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * research-insight service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::research-insight.research-insight');
