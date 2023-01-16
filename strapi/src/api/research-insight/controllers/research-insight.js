1f1a4e84aa307e5e91227f8bf5f31e803b2f124e
Thew Dhanat <26767607+ThewBear@users.noreply.github.com>
2023-01-16 18:57:41 +0700

    Strapi - Add research-insight collection (#786)
    Prepare for #782

---
+++ strapi/src/api/research-insight/controllers/research-insight.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * research-insight controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::research-insight.research-insight');
